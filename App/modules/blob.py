29d8da80b58bf6a1f6bcee5bd716ff93f2ae2f9e
reyi <130519102+mriduliyer@users.noreply.github.com>
2023-05-18 02:15:01 +0530

    Delete App directory

--- App/modules/blob.py
+++
@@ -1,6 +0,0 @@
-from azure.storage.blob import BlobServiceClient
-
-#Connect to Azure Blob
-connection_string = 'DefaultEndpointsProtocol=https;AccountName=storageaccpp2;AccountKey=clypY4aUF/1XPI4DqKnmrqv9mNkv+n9TIQxy9Q0j9YxRpK0TP0Y++AjhPuBC53N/Jh9tp4PWh6EZ+AStgfO8DQ==;EndpointSuffix=core.windows.net'        
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-container_name = 'storagecont'
--- App/modules/template.py
+++
@@ -1,4 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-
-# Create a Jinja2 environment with a file system loader
-env = Environment(loader=FileSystemLoader('.'))
